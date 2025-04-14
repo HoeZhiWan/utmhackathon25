c2fb7cfd12cbe93a73bfd7cbf98d66affbe4f590
Hoe Zhi Wan <hoezhiwan.study@gmail.com>
2025-04-14 14:31:46 +0800

    Initial Commit

---
+++ app/admin/page.tsx
@@ -0,0 +1,8 @@
+export default function AdminPage() {    
+    return (
+        <div>
+            <h1>Welcome to the Admin Page</h1>
+            <p>This is the main content of the admin page.</p>
+        </div>
+    );
+}
