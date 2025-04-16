36c1b4a38998f0f74d904e3ba370ba366af82c5b
Alfred Chin <alfredchin2005@gmail.com>
2025-04-17 00:38:39 +0800

    Change sidebar style and hover

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -1,36 +1,65 @@
+"use client";
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 const Sidebar = () => {
   const sidebarLinks = [
-    { href: '/', label: 'Dashboard', icon: '📊' },
-    { href: '/projects', label: 'Device', icon: '🚀' },
-    { href: '/teams', label: 'Solar Energy', icon: '👥' },
-    { href: '/resources', label: 'Overview', icon: '📚' },
-    { href: '/settings', label: 'Settings', icon: '⚙️' },
+    { href: '/',
+      label: 'Dashboard', 
+      icon: 
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-[20px] h-[20px]">
+          <path fillRule="evenodd" d="M3 6a3 3 0 0 1 3-3h2.25a3 3 0 0 1 3 3v2.25a3 3 0 0 1-3 3H6a3 3 0 0 1-3-3V6Zm9.75 0a3 3 0 0 1 3-3H18a3 3 0 0 1 3 3v2.25a3 3 0 0 1-3 3h-2.25a3 3 0 0 1-3-3V6ZM3 15.75a3 3 0 0 1 3-3h2.25a3 3 0 0 1 3 3V18a3 3 0 0 1-3 3H6a3 3 0 0 1-3-3v-2.25Zm9.75 0a3 3 0 0 1 3-3H18a3 3 0 0 1 3 3V18a3 3 0 0 1-3 3h-2.25a3 3 0 0 1-3-3v-2.25Z" clipRule="evenodd" />
+        </svg>
+     },
+    { href: '/projects', 
+      label: 'Device', 
+      icon: 
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-[20px] h-[20px]">
+          <path fillRule="evenodd" d="M14.615 1.595a.75.75 0 0 1 .359.852L12.982 9.75h7.268a.75.75 0 0 1 .548 1.262l-10.5 11.25a.75.75 0 0 1-1.272-.71l1.992-7.302H3.75a.75.75 0 0 1-.548-1.262l10.5-11.25a.75.75 0 0 1 .913-.143Z" clipRule="evenodd" />
+        </svg>
+     },
+    { href: '/teams', 
+      label: 'Solar Energy', 
+      icon: 
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-[20px] h-[20px]">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
+        </svg>
+    },
+    { href: '/settings', 
+      label: 'Settings',
+      icon: 
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-[20px] h-[20px]">
+          <path d="M6 12a.75.75 0 0 1-.75-.75v-7.5a.75.75 0 1 1 1.5 0v7.5A.75.75 0 0 1 6 12ZM18 12a.75.75 0 0 1-.75-.75v-7.5a.75.75 0 0 1 1.5 0v7.5A.75.75 0 0 1 18 12ZM6.75 20.25v-1.5a.75.75 0 0 0-1.5 0v1.5a.75.75 0 0 0 1.5 0ZM18.75 18.75v1.5a.75.75 0 0 1-1.5 0v-1.5a.75.75 0 0 1 1.5 0ZM12.75 5.25v-1.5a.75.75 0 0 0-1.5 0v1.5a.75.75 0 0 0 1.5 0ZM12 21a.75.75 0 0 1-.75-.75v-7.5a.75.75 0 0 1 1.5 0v7.5A.75.75 0 0 1 12 21ZM3.75 15a2.25 2.25 0 1 0 4.5 0 2.25 2.25 0 0 0-4.5 0ZM12 11.25a2.25 2.25 0 1 1 0-4.5 2.25 2.25 0 0 1 0 4.5ZM15.75 15a2.25 2.25 0 1 0 4.5 0 2.25 2.25 0 0 0-4.5 0Z" />
+        </svg>
+    },
   ];
 
+  const pathname = usePathname();
+
   return (
-    <aside className="bg-secondary-dark w-64 min-h-screen flex flex-col">
+    <aside className="bg-secondary-dark min-h-screen flex flex-col basis-1/4">
       {/* Sidebar header that aligns with navbar */}
-      <div className="p-3.5 bg-background flex items-center">
-        <Link href="/" className="text-xl font-bold text-light">UTMxHackathon</Link>
+      <div className="bg-background-dark flex items-center h-16 px-6 py-[9px] border-b-[3px] border-[rgba(0,0,0,0.25)]">
+        <Link href="/" className="text-2xl font-bold text-light">UTMxHackathon</Link>
       </div>
       
       {/* Sidebar content */}
       <div className="p-4 flex-1">
         <nav>
           <ul className="space-y-2">
-            {sidebarLinks.map((link, index) => (
+            {sidebarLinks.map((link, index) => {
+              const isActive = pathname === link.href;
+              return (
               <li key={index}>
                 <Link 
                   href={link.href} 
-                  className="flex items-center p-2 rounded-lg text-light hover:bg-primary-dark transition-colors"
+                  className={`flex box-border items-center px-8 py-4 rounded-lg text-light transition-colors ${isActive ? "border-1 bg-primary-neon-tint-400 border-primary-tint-500" : "hover:bg-primary-neon-tint-400"}`}
                 >
                   <span className="mr-3">{link.icon}</span>
                   {link.label}
                 </Link>
               </li>
-            ))}
+            )})}
           </ul>
         </nav>
       </div>
