df4e5d2d09dadc65c4fc2c529dde0c10f29f8fa7
Alfred Chin <alfredchin2005@gmail.com>
2025-04-17 21:55:24 +0800

    Add mockup style on first container

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
 export default function HomePage() {
   return (
     <div className="grid grid-cols-3 grid-rows-3 gap-4 h-full">
-      <div className="bg-shade-500">
-        <div>01</div>
+      <div className="bg-shade-500 rounded-xl p-4 flex flex-col min-w-[362px]">
+        <div className="text-[20px]">Total Cost</div>
+        <div className="flex justify-between text-[38px]">
+          RM 15,000.00
+          <div className="text-[16px]">19.6%</div>
+        </div>
+        <div>
+          <div>View Tariff</div>
+        </div>
       </div>
-      <div className="bg-shade-500">
+      <div className="bg-shade-500 min-w-[362px]">
         <div>02</div>
       </div>
       <div className="bg-shade-500">
